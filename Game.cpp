ef951a16e09bae208fdd5697f03429441c356c2c
창훈 박 <41935668+wawworld72@users.noreply.github.com>
2021-06-26 08:28:58 +0000

    2차 alpha 이미지

--- Game.cpp
+++ Game.cpp
@@ -21,7 +21,7 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
 
   m_bRunning = true;
 
-  SDL_Surface* pTempSurface = IMG_Load("Assets/animate.png");
+  SDL_Surface* pTempSurface = IMG_Load("Assets/animate-alpha.png");
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
   SDL_FreeSurface(pTempSurface);
 
