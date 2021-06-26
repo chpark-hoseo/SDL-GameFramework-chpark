51be23090c0fa8725378dd6f4bfd53ef2be18a8a
창훈 박 <41935668+wawworld72@users.noreply.github.com>
2021-06-26 08:25:49 +0000

    1차 SDL_image

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "SDL_image.h"
 
 bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags) {
   if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
@@ -7,7 +8,7 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
       m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 
       if (m_pRenderer != 0) {
-        SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255); 
       } else {
         return false; // 랜더러 생성 실패
       }
@@ -20,7 +21,7 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
 
   m_bRunning = true;
 
-  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");  
+  SDL_Surface* pTempSurface = IMG_Load("Assets/animate.png");
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
   SDL_FreeSurface(pTempSurface);
 
