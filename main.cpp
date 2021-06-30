7eb4bac8688ae6af991854dd7bbf99d5ff31e138
창훈 박 <41935668+wawworld72@users.noreply.github.com>
2021-06-30 12:33:27 +0000

    고정 프레임 갱신

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include "Game.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main(int argc, char* argv[])
 {
+
+
   if (TheGame::Instance()->init("Game Class", 
                100, 100, 640, 480, false)) 
   {
-      while (TheGame::Instance()->running()) {
+    Uint32 frameStart, frameTime;
+    while (TheGame::Instance()->running()) {
+      frameStart = SDL_GetTicks();     // step 1
+
      	TheGame::Instance()->handleEvents();
-      	TheGame::Instance()->update();
-      	TheGame::Instance()->render();
-      	SDL_Delay(10);
+      TheGame::Instance()->update();
+      TheGame::Instance()->render();
+
+      frameTime = SDL_GetTicks() - frameStart;  // step 2
+      if (frameTime < DELAY_TIME) {
+        SDL_Delay((int)(DELAY_TIME - frameTime)); // step 3
       }
+    }
   }  else {
       printf("game init failure %s\n" , SDL_GetError());
       return -1;
